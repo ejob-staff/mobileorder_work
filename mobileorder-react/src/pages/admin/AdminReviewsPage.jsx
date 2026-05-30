94973be6ce920b9631f8ccbbbbf8286e14cc366c
kousei kawatani <kawaniko1118@gmail.com>
2026-05-30 21:04:12 +0900

    main edit source

--- mobileorder-react/src/pages/admin/AdminReviewsPage.jsx
+++ mobileorder-react/src/pages/admin/AdminReviewsPage.jsx
@@ -41,7 +41,7 @@ export default function AdminReviewsPage({ reviews, onLoadReviews }) {
                 <div className="review-detail-list">
                   <span>注文番号: {review.orderNumber}</span>
                   <span>ユーザー名: {review.username}</span>
-                  <span>評価:{' '}<RatingStars rating={review.rating} /></span>
+                  <span>評価: <RatingStars rating={review.rating} /></span>
                 </div>
                 <p className="review-comment">
                   <span>お客様の感想</span>
